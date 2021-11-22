6b5c1b092a7409031e56a3e84e846cd5af9e14ad
Maged Mahmoud Adel <magedmahmoud94@outlook.com>
2021-11-22 22:50:07 +0200

    Same as previous commit, it didn't add all the files last time

---
+++ src/Components/Page.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import BookShelf from './BookShelf';
+
+
+class Page extends Component{
+    render(){
+        return(
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>          
+                <BookShelf title='Currently Reading'/>
+                <BookShelf title='Want to Read'/>
+                <BookShelf title='Read'/>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to='/search'>Add a book</Link>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Page;
